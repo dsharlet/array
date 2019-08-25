57230dfdde5eebcfd9eead6252577a48c32417fc
Dillon Sharlet <dsharlet@gmail.com>
2019-08-24 22:44:54 -0700

    Add more tests of array.

--- test/array.cpp
+++ test/array.cpp
@@ -3,30 +3,136 @@
 
 namespace array {
 
-TEST(array_1d) {
-  auto A = make_dense_array<int>(10);
-  for_all_indices(A.shape(), [&](int x) {
-    A(x) = x;
-  });
-
-  auto B = make_dense_array<int>(10);
-  B = A;
-  for_all_indices(B.shape(), [&](int x) {
-    ASSERT_EQ(B(x), x);
-  });
+shape<dim<>, dim<>> make_2d_shape(const dim<>& x, const dim<>& y) {
+  return {x, y};
 }
 
-TEST(array_2d) {
-  auto A = make_dense_array<int>(10, 5);
-  for_all_indices(A.shape(), [&](int x, int y) {
-    A(x, y) = y * 100 + x;
-  });
+typedef array<int, shape<dense_dim<>>> array_1d;
+typedef array<int, shape<dense_dim<>, dim<>>> array_2d;
+typedef array<int, shape<dense_dim<>, dim<>, dim<>>> array_3d;
 
-  auto B = make_dense_array<int>(10, 5);
-  B = A;
-  for_all_indices(B.shape(), [&](int x, int y) {
-    ASSERT_EQ(B(x, y), y * 100 + x);
-  });  
+TEST(array_default_constructor) {
+  array_1d a(make_dense_shape(10));
+  for (int x = 0; x < 10; x++) {
+    ASSERT_EQ(a(x), 0);
+  }
+
+  array_2d b(make_dense_shape(7, 3));
+  for (int y = 0; y < 3; y++) {
+    for (int x = 0; x < 7; x++) {
+      ASSERT_EQ(b(x, y), 0);
+    }
+  }
+
+  array_3d c(make_dense_shape(5, 9, 3));
+  for (int z = 0; z < 3; z++) {
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 5; x++) {
+        ASSERT_EQ(c(x, y, z), 0);
+      }
+    }
+  }
+
+  auto sparse_shape = make_shape(dim<>(-2, 5, 2), dim<>(4, 10, 20));
+  array<int, shape<dim<>, dim<>>> sparse(sparse_shape);
+  for (int y = 4; y < 14; y++) {
+    for (int x = -2; x < 3; x++) {
+      ASSERT_EQ(sparse(x, y), 0);
+    }
+  }
+}
+
+TEST(array_fill_constructor) {
+  array_1d a(make_dense_shape(10), 3);
+  for (int x = 0; x < 10; x++) {
+    ASSERT_EQ(a(x), 3);
+  }
+
+  array_2d b(make_dense_shape(7, 3), 5);
+  for (int y = 0; y < 3; y++) {
+    for (int x = 0; x < 7; x++) {
+      ASSERT_EQ(b(x, y), 5);
+    }
+  }
+
+  array_3d c(make_dense_shape(5, 9, 3), 7);
+  for (int z = 0; z < 3; z++) {
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 5; x++) {
+        ASSERT_EQ(c(x, y, z), 7);
+      }
+    }
+  }
+
+  auto sparse_shape = make_shape(dim<>(-2, 5, 2), dim<>(4, 10, 20));
+  array<int, shape<dim<>, dim<>>> sparse(sparse_shape, 13);
+  for (int y = 4; y < 14; y++) {
+    for (int x = -2; x < 3; x++) {
+      ASSERT_EQ(sparse(x, y), 13);
+    }
+  }
+}
+
+TEST(array_fill_assign) {
+  array_1d a;
+  a.assign(make_dense_shape(10), 3);
+  for (int x = 0; x < 10; x++) {
+    ASSERT_EQ(a(x), 3);
+  }
+
+  array_2d b;
+  b.assign(make_dense_shape(7, 3), 5);
+  for (int y = 0; y < 3; y++) {
+    for (int x = 0; x < 7; x++) {
+      ASSERT_EQ(b(x, y), 5);
+    }
+  }
+
+  array_3d c;
+  c.assign(make_dense_shape(5, 9, 3), 7);
+  for (int z = 0; z < 3; z++) {
+    for (int y = 0; y < 9; y++) {
+      for (int x = 0; x < 5; x++) {
+        ASSERT_EQ(c(x, y, z), 7);
+      }
+    }
+  }
+
+  array<int, shape<dim<>, dim<>>> sparse;
+  auto sparse_shape = make_shape(dim<>(-2, 5, 2), dim<>(4, 10, 20));
+  sparse.assign(sparse_shape, 13);
+  for (int y = 4; y < 14; y++) {
+    for (int x = -2; x < 3; x++) {
+      ASSERT_EQ(sparse(x, y), 13);
+    }
+  }
+}
+
+TEST(sparse_array) {
+  auto sparse_shape = make_shape(dim<>(-2, 5, 2), dim<>(4, 10, 20));
+  array<int, shape<dim<>, dim<>>> sparse(sparse_shape);
+  // Fill the storage with a constant.
+  for (int i = 0; i < sparse_shape.flat_extent(); i++) {
+    sparse.data()[i] = 7;
+  }
+  // Assign a different constant.
+  sparse.assign(sparse_shape, 3);
+
+  // Check that we assigned all of the elements of the array.
+  for (int y = 4; y < 14; y++) {
+    for (int x = -2; x < 3; x++) {
+      ASSERT_EQ(sparse(x, y), 3);
+    }
+  }
+
+  // Check that only the elements of the array were assigned.
+  int threes = 0;
+  for (int i = 0; i < sparse_shape.flat_extent(); i++) {
+    if (sparse.data()[i] == 3) {
+      threes++;
+    }
+  }
+  ASSERT_EQ(threes, sparse.size());
 }
 
 }  // namespace array
