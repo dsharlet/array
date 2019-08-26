ce494399d190223b69587f154b45a405633edb59
Dillon Sharlet <dsharlet@gmail.com>
2019-08-26 08:47:02 -0700

    Simplify matrix arrays.

--- test/matrix_multiply.cpp
+++ test/matrix_multiply.cpp
@@ -3,16 +3,8 @@
 
 namespace array {
 
-typedef shape<dim<>, dense_dim<>> matrix_shape;
-
 template <typename T>
 using matrix = array<T, shape<dim<>, dense_dim<>>>;
-
-template <typename T>
-matrix<T> make_matrix(int M, int N) {
-  matrix_shape shape({0, M, N}, {0, N});
-  return matrix<T>(shape, 0);
-}
 
 template <typename T>
 const dim<>& rows(const matrix<T>& m) { return m.shape().template dim<0>(); }
@@ -47,14 +39,14 @@ void multiply(const matrix<T>& a, const matrix<T>& b, matrix<T>& c) {
 }
 
 TEST(matrix_multiply) {
-  auto a = make_matrix<float>(10, 20);
-  auto b = make_matrix<float>(20, 30);
-  auto c = make_matrix<float>(10, 30);
+  matrix<float> a({10, 20});
+  matrix<float> b({20, 30});
+  matrix<float> c({10, 30});
   a.for_each_value([](float& x) { x = rand(); });
   b.for_each_value([](float& x) { x = rand(); });
   multiply(a, b, c);
 
-  auto c_ref = make_matrix<float>(10, 30);
+  matrix<float> c_ref({10, 30});
   multiply_naive(a, b, c_ref);
   
   for (int i = 0; i < 10; i++) {
