645f496a9884f3803dfa74207b8c6b4455cf0f0b
Jiawen Chen <jiawen@adobe.com>
2023-01-04 09:00:47 -0800

    Remove default template arguments. Use StrFormat("%v") and StrJoin.

--- absl/absl.h
+++ absl/absl.h
@@ -1,4 +1,5 @@
 #include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
 #include "array.h"
 
 // Adds Abseil Stringify support (https://abseil.io/blog/20221115-stringify).
@@ -8,17 +9,16 @@ namespace nda {
 // interval -> string as the half-open interval [begin, end).
 //
 // Stringifies only the values, not whether they are static or dynamic.
-template <typename Sink, index_t Min_ = dynamic, index_t Extent_ = dynamic>
-void AbslStringify(Sink& sink, const interval<Min_, Extent_>& i) {
+template <typename Sink, index_t Min, index_t Extent>
+void AbslStringify(Sink& sink, const interval<Min, Extent>& i) {
   absl::Format(&sink, "[%v, %v)", i.min(), i.min() + i.extent());
 }
 
 // dim -> string as "dim(min, extent, stride)".
 //
 // Stringifies only the values, not whether they are static or dynamic.
-template <typename Sink, index_t Min_ = dynamic, index_t Extent_ = dynamic,
-    index_t Stride_ = dynamic>
-void AbslStringify(Sink& sink, const dim<Min_, Extent_, Stride_>& d) {
+template <typename Sink, index_t Min, index_t Extent, index_t Stride>
+void AbslStringify(Sink& sink, const dim<Min, Extent, Stride>& d) {
   absl::Format(&sink, "dim(%v, %v, %v)", d.min(), d.extent(), d.stride());
 }
 
@@ -27,19 +27,11 @@ void AbslStringify(Sink& sink, const dim<Min_, Extent_, Stride_>& d) {
 // Stringifies only the rank and each dim's values, not whether they are static
 // or dynamic.
 //
-// TODO(jiawen): This can be implemented at compile-time using index_sequence
-// (and a fold expression in C++17) but is not worth the reduced readability.
+// While this can be implemented at compile-time, it is not worth the code size
+// and reduced readability.
 template <typename Sink, class... Dims>
 void AbslStringify(Sink& sink, const shape<Dims...>& sh) {
-  absl::Format(&sink, "shape<%d>(", sh.rank());
-  for (int i = 0; i < sh.rank(); ++i) {
-    if (i == 0) {
-      absl::Format(&sink, "%v", sh.dim(i));
-    } else {
-      absl::Format(&sink, ", %v", sh.dim(i));
-    }
-  }
-  sink.Append(")");
+  absl::Format(&sink, "shape<%d>(%v)", sh.rank(), absl::StrJoin(sh.dims(), ", "));
 }
 
 } // namespace nda
