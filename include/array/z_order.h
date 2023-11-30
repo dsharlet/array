ff2a2848b7fdb7441e358abc80665f8088c9f27c
Dillon <dsharlet@gmail.com>
2023-11-29 20:44:24 -0800

    Pass z by value to avoid save/restore, makes calls tail recursive(?)

--- include/array/z_order.h
+++ include/array/z_order.h
@@ -39,26 +39,23 @@ inline index_t next_power_of_two(index_t x) {
 // to use std::function instead.
 template <size_t Rank, class Fn>
 NDARRAY_UNIQUE void for_each_index_in_z_order_impl(const std::array<index_t, Rank>& end,
-    std::array<index_t, Rank>& z, index_t dim, index_t step, const Fn& fn) {
+    std::array<index_t, Rank> z, index_t dim, index_t step, const Fn& fn) {
   if (dim == 0 && step == 1) {
     // We're at the innermost step of the traversal, call fn for the two neighboring points we have.
     fn(z);
     z[0] += 1;
     if (z[0] < end[0]) fn(z);
-    z[0] -= 1;
   } else if (dim == 0) {
     // We're on the innermost dimension, but not the innermost step.
     // Move to the next step for the outermost dimension.
     for_each_index_in_z_order_impl(end, z, Rank - 1, step >> 1, fn);
     z[0] += step;
     if (z[0] < end[0]) for_each_index_in_z_order_impl(end, z, Rank - 1, step >> 1, fn);
-    z[0] -= step;
   } else {
     // Move to the next dimension for the same step.
     for_each_index_in_z_order_impl(end, z, dim - 1, step, fn);
     z[dim] += step;
     if (z[dim] < end[dim]) for_each_index_in_z_order_impl(end, z, dim - 1, step, fn);
-    z[dim] -= step;
   }
 }
 
@@ -72,16 +69,16 @@ NDARRAY_UNIQUE void for_each_index_in_z_order(const Extents& extents, const Fn& 
   const index_t max_extent = *std::max_element(end.begin(), end.end());
   const index_t step = std::max<index_t>(1, internal::next_power_of_two(max_extent) >> 1);
   std::array<index_t, Rank> z = {{0,}};
-  internal::for_each_index_in_z_order_impl(end, z, Rank - 1, step, [&](const std::array<index_t, Rank>& i) { fn(i); });
+  for_each_index_in_z_order_impl(end, z, Rank - 1, step, [&](const std::array<index_t, Rank>& i) { fn(i); });
 }
 
 template <class... Ranges, class Fn, size_t... Is>
 NDARRAY_UNIQUE void for_each_in_z_order_impl(
     const std::tuple<Ranges...>& ranges, Fn&& fn, internal::index_sequence<Is...>) {
-  constexpr size_t rank = sizeof...(Is);
+  constexpr size_t Rank = sizeof...(Is);
   // TODO: Use std::size if we switch to C++17
-  std::array<index_t, rank> extents = {{std::get<Is>(ranges).size()...}};
-  for_each_index_in_z_order(extents, [fn = std::move(fn), &ranges](const auto& i) {
+  std::array<index_t, Rank> extents = {{std::get<Is>(ranges).size()...}};
+  for_each_index_in_z_order(extents, [fn = std::move(fn), &ranges](const std::array<index_t, Rank>& i) {
     fn(std::make_tuple(*(std::begin(std::get<Is>(ranges)) + std::get<Is>(i))...));
   });
 }
